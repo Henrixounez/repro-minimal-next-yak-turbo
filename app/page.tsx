57274e8a083dea5374124998eed7de59e0b9051b
Henrixounez <henrix.ounez@yahoo.fr>
2025-11-27 17:09:09 +0100

    feat: use :global() selector

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import { styled } from "next-yak";
 
-const StyledH1 = styled.h1`
+const Container = styled.div`
   color: red;
+
+  > :global(.text) {
+   color: blue;
+  }
 `
 
 export default function Page() {
-  return <StyledH1>Hello, Next.js!</StyledH1>;
+  return (
+  <Container>
+    <p className="text">
+      Hello, Next.js!
+    </p>
+    </Container>);
 }
